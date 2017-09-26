53ed47f8a0b44c83bee6dde1fa7011411e7d618d
alexander <barmin1@mail.ru>
2017-09-26 17:54:25 +0300

    Similarity

---
+++ Model/NeuralNetwork/Metrics.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def similarity(img1, img2, percent=False):
+    width = img1.shape[0]
+    height = img1.shape[1]
+    sim = np.sum(img1==img2)
+    if len(img1.shape) == 2:
+        img1 = np.reshape(img1, newshape=(width,height,1))
+    ch = img1.shape[2]
+    if percent:
+        sim = (sim/(width*height*ch))*100
+    return sim
--- View/NNView.py
+++ View/NNView.py
@@ -1,9 +1,11 @@
-from PyQt4.QtGui import QMainWindow, QDoubleValidator, QFileDialog, QTreeWidgetItem
+from PyQt4.QtGui import QMainWindow, QDoubleValidator, QFileDialog, QTreeWidgetItem, QPixmap
 from PyQt4.QtCore import SIGNAL
 from Utility.NNModelObserver import NNModelObserver
 from Utility.NNMeta import NNMeta
 from View.MainWindow import Ui_MainWindow
 from PyQt4.Qt import QStringListModel
+from scipy.ndimage import imread
+from Model.NeuralNetwork.Metrics import similarity
 
 class NNView(QMainWindow, NNModelObserver, metaclass=NNMeta):
 
@@ -22,8 +24,16 @@ class NNView(QMainWindow, NNModelObserver, metaclass=NNMeta):
         #Добавляем обработчики событий
         self.ui.class_1.clicked.connect(self.addDataX)
         self.ui.class_2.clicked.connect(self.addDataY)
+        self.ui.test_class.clicked.connect(self.addTest)
 
         self.treeSettings()
+        self.ui.img_1.setScaledContents(True)
+        self.ui.img_2.setScaledContents(True)
+        self.ui.img_3.setScaledContents(True)
+
+        self.current_class_1 = None
+        self.current_class_2 = None
+        self.current_test = None
 
     def treeSettings(self):
         self.item1 = QTreeWidgetItem(['Класс 1'])
@@ -34,13 +44,41 @@ class NNView(QMainWindow, NNModelObserver, metaclass=NNMeta):
         self.ui.data_tree.addTopLevelItem(self.item3)
         self.connect(self.ui.data_tree, SIGNAL("itemClicked(QTreeWidgetItem*, int)"), self.onClickItem)
 
-    def onClickItem(self, item, column):
-        path = item.text(column)
-        parent = item.parent().text(0)
 
 
     def modelIsChanged(self):
         pass
+
+    def onClickItem(self, item, column):
+        try:
+            path = item.text(column)
+            parent = item.parent().text(0)
+            if parent=='Класс 1':
+                img = imread(path)
+                self.ui.img_1.setPixmap(QPixmap(path))
+                self.current_class_1 = imread(path)
+                if self.current_test is not None:
+                    sim_1 = similarity(img, self.current_test)
+                    self.ui.sim_1.setText(str(sim_1))
+            if parent=='Класс 2':
+                img = imread(path)
+                self.ui.img_2.setPixmap(QPixmap(path))
+                self.current_class_2 = imread(path)
+                if self.current_test is not None:
+                    sim_2 = similarity(img, self.current_test)
+                    self.ui.sim_2.setText(str(sim_2))
+            if parent=='Тест':
+                self.ui.img_3.setPixmap(QPixmap(path))
+                img = imread(path)
+                self.current_test = img
+                if self.current_class_1 is not None:
+                    sim_1 = similarity(img, self.current_class_1)
+                    self.ui.sim_1.setText(str(sim_1))
+                if self.current_class_2 is not None:
+                    sim_2 = similarity(img, self.current_class_2)
+                    self.ui.sim_2.setText(str(sim_2))
+        except:
+            pass
 
     def addDataX(self):
         dlg = QFileDialog()
@@ -61,3 +99,13 @@ class NNView(QMainWindow, NNModelObserver, metaclass=NNMeta):
             filenames = dlg.selectedFiles()
         for f in filenames:
             self.item2.addChild(QTreeWidgetItem([str(f)]))
+
+    def addTest(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setFileMode(QFileDialog.ExistingFiles)
+        filenames = QStringListModel()
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+        for f in filenames:
+            self.item3.addChild(QTreeWidgetItem([str(f)]))
